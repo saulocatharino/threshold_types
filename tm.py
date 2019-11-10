78137dd0003fc103fb6dfb2807de6b0d9c873c5f
Saulo Catharino <saulocatharino@gmail.com>
2019-11-10 14:09:25 -0400

    Update tm.py

--- tm.py
+++ tm.py
@@ -1,9 +1,6 @@
 import cv2
 import numpy as np
 
-
-image = cv2.imread('mao.jpg')
-image = cv2.resize(image,None,fx=.3,fy=.3)
 cap = cv2.VideoCapture(1)
 
 
@@ -20,7 +17,6 @@ while True:
   
   f1 = cv2.hconcat([thresh1,thresh2,thresh3])
   f2 = cv2.hconcat([thresh4,thresh5, img])
-  #im[:image.shape[0],:image.shape[1]]-= image[:image.shape[0],:image.shape[1]]
   f3 = cv2.vconcat([f1,f2])
   cv2.imshow("frame", f3)
   k = cv2.waitKey(1)
